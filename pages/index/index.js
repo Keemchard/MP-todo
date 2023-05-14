205ebfac21a37309b81581eb06d02b1b5b7c7b77
Keemchard <keemchardtamio@gmail.com>
2023-05-14 19:33:41 +0800

    comment stuff

--- pages/index/index.js
+++ pages/index/index.js
@@ -10,30 +10,25 @@ const setTodos = (todos)=>{
 
 Page({ 
   data: {globalTodos},
+
   onSubmit(e) {
     const {__unknown_for_control_1: todoTitle} = e.detail.value;;
     let newSetOfTodos = [...globalTodos, {id: Math.random(), title: todoTitle}]
-
     setTodos(newSetOfTodos)
-    console.log(setTodos(newSetOfTodos))
- 
-    
-    // this.setData({ globalTodos: newSetOfTodos }, console.log(newSetOfTodos)) 
-
-    
+    this.setData({ globalTodos: newSetOfTodos }) 
   }, 
+
   deleteTodo(event){ 
     const id = event.target.dataset.id;
     const filteredTodos = globalTodos.filter((todo)=>{
         return id !== todo.id ; 
     })
-    console.log(id)
-    console.log(filteredTodos)
     setTodos(filteredTodos)
     this.setData({globalTodos: filteredTodos})
   }
+  
   // deleteAll() {
   //   my.clearStorageSync();
-  //   console.log(globalTodos)
+  //   console.log(globalTodos) 
   // }
 });
